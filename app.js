33f0a7a2cf93312d2879c939f2764c41c9f1a07a
lucaspedrosoti <lps1010@gmail.com>
2020-03-12 15:45:32 -0300

    integrada as alterações do Mauricio na homepage

--- app.js
+++ app.js
@@ -7,6 +7,8 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 const loginRouter = require('./routes/login');
+const mainRouter = require('./routes/main');
+const accountRouter = require('./routes/account');
 
 var app = express();
 
@@ -23,6 +25,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/login', loginRouter);
+app.use('/main', mainRouter);
+app.use('/account', accountRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
