21aa5b77e53b01495ccc2341dc2ffbec50953028
Mauricio <mauricio.ssr@hotmail.com>
2020-03-18 11:38:46 -0300

    Feito tela de cadastro pessoa juridica, adicionado rota

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ var usersRouter = require('./routes/users');
 const loginRouter = require('./routes/login');
 const mainRouter = require('./routes/main');
 const accountRouter = require('./routes/account');
+const cadastrocnpjRouter = require('./routes/cadastrocnpj');
 
 var app = express();
 
@@ -31,23 +32,22 @@ app.use('/users', usersRouter);
 app.use('/login', loginRouter);
 app.use('/main', mainRouter);
 app.use('/account', accountRouter);
-
-
+app.use('/cadastrocnpj', cadastrocnpjRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-    next(createError(404));
+  next(createError(404));
 });
 
 // error handler
 app.use(function(err, req, res, next) {
-    // set locals, only providing error in development
-    res.locals.message = err.message;
-    res.locals.error = req.app.get('env') === 'development' ? err : {};
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-    // render the error page
-    res.status(err.status || 500);
-    res.render('error');
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
 });
 
 module.exports = app;
